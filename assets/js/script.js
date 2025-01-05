aca69ac60e70fdadd39a23a28ab10b1053b8bc24
Akkenapally Ruthwik Reddy <131450727+ruthwwikreddy@users.noreply.github.com>
2025-01-05 21:29:54 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
 'use strict';
 
+// Google Tag Manager Integration
+(function(w, d, s, l, i) {
+  w[l] = w[l] || [];
+  w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+  var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != 'dataLayer' ? '&l=' + l : '';
+  j.async = true;
+  j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+  f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-PWKV7K8Q');
 
 
 // element toggle function
@@ -277,4 +288,3 @@ function openFullscreen(element) {
       modal.style.display = "none";
     }
   }
-}
