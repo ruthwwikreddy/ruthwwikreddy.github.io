a24cff0cf82218c90f50dc300084a03f0d4125e7
Akkenapally Ruthwik Reddy <131450727+ruthwwikreddy@users.noreply.github.com>
2025-01-09 21:16:24 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,5 @@
 'use strict';
 
-// Google Tag Manager Integration
-(function(w, d, s, l, i) {
-  w[l] = w[l] || [];
-  w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-  var f = d.getElementsByTagName(s)[0],
-      j = d.createElement(s),
-      dl = l != 'dataLayer' ? '&l=' + l : '';
-  j.async = true;
-  j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-  f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-PWKV7K8Q');
-
-
 // element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
